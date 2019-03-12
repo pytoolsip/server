c539392fc5907547c35034072d10e3971fe447ae
JinZhang <2636423765@qq.com>
2019-03-12 19:51:43 +0800

    feat:添加依赖组件解析逻辑

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 # @Author: JimZhang
 # @Date:   2019-02-23 12:36:46
-# @Last Modified by:   JimDreamHeart
-# @Last Modified time: 2019-03-06 00:06:40
-import os,time;
+# @Last Modified by:   JinZhang
+# @Last Modified time: 2019-03-12 19:50:34
+import os,time,json;
 
+# 安装依赖模块
+dependJson = os.path.abspath(os.path.join(os.path.dirname(__file__),"../depend.json"));
+if os.path.exists(dependJson):
+	with open(dependJson, "r") as f:
+		dependList = json.loads(f.read());
+		for depend in dependList:
+			if os.system("pip show " + depend) == 1:
+				os.system("pip install " + depend);
+
+# 加载工程
 from _load import Loader;
-from _Global import _GG;
-
-# 加载全局变量
-Loader = Loader(os.getcwd());
-Loader.loadGlobalInfo();
+Loader = Loader(os.getcwd()); # 获取加载器
+Loader.loadGlobalInfo(); # 加载全局变量
 MainServer = Loader.loadMainServer(); # 加载主服务
 Log = Loader.loadLogger(); # 加载日志
 Loader.lockGlobal_G(); # 锁定全局变量
