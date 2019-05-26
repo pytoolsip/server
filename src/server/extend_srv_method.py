e8147530e07fbade717f7aac690e9918c84251aa
JimDreamHeart <15602291936@163.com>
2019-05-26 18:16:18 +0800

    Revert "refactor:修改从数据库中获取用户名的逻辑"
    This reverts commit 63cf448d3da9dd744202457d37cd77d1b7478468.

--- src/server/extend_srv_method.py
+++ src/server/extend_srv_method.py
@@ -22,28 +22,18 @@ def ExtendSrvMethod():
 	pass;
 
 def RequestToolInfos(data, context):
-	sql = "SELECT tool.name, tool.category, tool.description, tkey, version, changelog FROM tool_detail LEFT OUTER JOIN tool ON tool_detail.tkey = tool.tkey WHERE common_version = '%s' ORDER BY time"%data.get("commonVersion", "");
+	sql = "SELECT tool.name, tool.category, tool.description, tkey, version, changelog, user.name FROM tool_detail LEFT OUTER JOIN tool ON tool_detail.tkey = tool.tkey LEFT OUTER JOIN user ON tool.uid = user.id WHERE common_version = '%s' ORDER BY time"%data.get("commonVersion", "");
 	ret, retData = _GG("DBCManager").MySQL().execute(sql);
 	if ret:
-		toolInfos, userNameMap = [], {};
-		for info in retData:
-			tkey = info["tkey"];
-			# 获取用户名
-			if tkey not in userNameMap:
-				ret, retData1 = _GG("DBCManager").MySQL().execute("SELECT user.name FROM tool LEFT OUTER JOIN user ON tool.uid = user.id WHERE tkey = '%s'"%tkey);
-				if ret:
-					userNameMap[tkey] = retData1[0]["user.name"];
-			# 添加工具信息
-			toolInfos.append({
-				"key" : tkey,
-				"title" : info["tool.name"],
-				"category" : info["tool.category"],
-				"description" : info["tool.description"],
-				"version" : info["version"],
-				"changelog" : info["changelog"],
-				"author" : userNameMap.get(tkey, "无"),
-			});
-		return True, toolInfos;
+		return True, [{
+			"key" : info["tkey"],
+			"title" : info["tool.name"],
+			"category" : info["tool.category"],
+			"description" : info["tool.description"],
+			"version" : info["version"],
+			"changelog" : info["changelog"],
+			"author" : info["user.name"],
+		} for info in retData];
 	return False, [];
 
 def VertifyToolName(data, context):
@@ -106,15 +96,9 @@ def sendEmailContent(email, title, content):
 	return False;
 
 def RequestToolInfo(data, context):
-	sql = "SELECT tool.name, tool.category, tool.description, version, changelog FROM tool_detail LEFT OUTER JOIN tool ON tool_detail.tkey = tool.tkey WHERE tkey = '%s' AND common_version = '%s' ORDER BY time"%(data.get("key", ""), data.get("commonVersion", ""));
+	sql = "SELECT tool.name, tool.category, tool.description, version, changelog, user.name FROM tool_detail LEFT OUTER JOIN tool ON tool_detail.tkey = tool.tkey LEFT OUTER JOIN user ON tool.uid = user.id WHERE tkey = '%s' AND common_version = '%s' ORDER BY time"%(data.get("key", ""), data.get("commonVersion", ""));
 	ret, retData = _GG("DBCManager").MySQL().execute(sql);
 	if ret:
-		# 获取用户名
-		userName = "无";
-		ret, retData1 = _GG("DBCManager").MySQL().execute("SELECT user.name FROM tool LEFT OUTER JOIN user ON tool.uid = user.id WHERE tkey = '%s'"%data.get("key", ""));
-		if ret:
-			userName = retData1[0]["user.name"];
-		# 返回工具信息
 		info = retData[0];
 		return True, {
 			"title" : info["tool.name"],
@@ -122,6 +106,6 @@ def RequestToolInfo(data, context):
 			"description" : info["tool.description"],
 			"version" : info["version"],
 			"changelog" : info["changelog"],
-			"author" : userName,
+			"author" : info["user.name"],
 		};
 	return False, {};
