333ed263b737600750c488b5f71f0467fcc4d309
JimDreamHeart <15602291936@163.com>
2019-09-13 15:13:36 +0800

    refactor:新增链接redis的密码项

--- src/dbc/RedisDB.py
+++ src/dbc/RedisDB.py
@@ -12,7 +12,7 @@ class RedisDB(object):
 	def __init__(self):
 		super(RedisDB, self).__init__();
 		srvConf = _GG("ServerConfig").Config(); # 服务配置
-		self.__pool = redis.ConnectionPool(host = srvConf.Get("redis", "host"), port = int(srvConf.Get("redis", "port")), decode_responses=True);
+		self.__pool = redis.ConnectionPool(host = srvConf.Get("redis", "host"), port = int(srvConf.Get("redis", "port")), password = srvConf.Get("redis", "password"), decode_responses=True);
 
 	def __call__(self):
 		return redis.Redis(connection_pool = self.__pool);
